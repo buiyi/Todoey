925cebda8e89a5a43c7a2cc1d2799567c55c3391
Murat Gürbüzdal <murat@Murat-Mac.local>
2018-11-07 20:54:50 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Murat Gürbüzdal on 7.11.2018.
+//  Copyright © 2018 Murat Gürbüzdal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
